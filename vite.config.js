59a5ccd00dbf71bd2330e808af98fe9c7dda2750
Varshith Pawar H R <varshithpawarhr@gmail.com>
2025-05-23 20:52:03 +0530

    fix: prod bugs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
 // base ia added just for gh-pages to work remove is gh-pages is not used
 export default defineConfig({
   plugins: [react()],
-  base: "/prod/"
-})
+  base: "./",
+});
